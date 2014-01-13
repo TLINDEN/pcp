7883de06bfba75672cb826db5c52f4d4ea7a912b
TLINDEN <git@daemon.de>
2014-01-13 18:52:26 +0100

    start help text on same line as define

--- src/pcp.h
+++ src/pcp.h
@@ -76,9 +76,8 @@
 0x00028F70
 */
 
-#define PCP_HELP_INTRO \
-"This is Pretty Curved Privacy. Licensed under the GPLv3. This\n"\
-"is BETA software. Use with care. NOT intended for production\n"\
+#define PCP_HELP_INTRO "This is Pretty Curved Privacy. Licensed under the GPLv3. This\n" \
+"is BETA software. Use with care. NOT intended for production\n"  \
 "use.\n"
 
 vault_t *vault;

0851f04cfc67f4cb6d4d51f33634bace88b4e093
TLINDEN <git@daemon.de>
2015-05-30 19:46:59 +0200

    re-generated, were incomplete

--- autogen.sh
+++ autogen.sh
@@ -60,11 +60,8 @@ Pretty Curved Privacy - File encryption using eliptic curve cryptography.
     (cd src && ./usage.sh)
 
     # generate pypcp types
-    bindings/py/gencffi.pl include/pcp/defines.h include/pcp/structs.h include/pcp/key.h \
-	                   include/pcp/buffer.h include/pcp/context.h \
-	                   include/pcp/ed.h include/pcp/crypto.h include/pcp/vault.h \
-	                   include/pcp/mgmt.h include/pcp/keyhash.h \
-	                   include/pcp/pcpstream.h include/pcp/z85.h > bindings/py/pypcp/raw.py
+    cd bindings/py
+    ./gencffi.pl ../../include/pcp/*.h > pypcp/raw.py
     cd -
 
     exit

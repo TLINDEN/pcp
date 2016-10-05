8235272c415b50b99f533db221cf3440687aeef1
git@daemon.de <git@daemon.de>
2016-10-05 13:34:09 +0200

    rm unneeded defs

--- include/pcp/ed.h
+++ include/pcp/ed.h
@@ -32,28 +32,6 @@
 #include <sodium.h>
 #include <string.h>
 #include <stdio.h>
-
-#if defined __linux__ || defined __GNU__ || defined __GLIBC__
-#  ifndef _DEFAULT_SOURCE
-#    define _DEFAULT_SOURCE 1
-#  endif
-#
-#  ifndef _XOPEN_SOURCE
-#    define _XOPEN_SOURCE 1
-#  endif
-#
-#  ifndef _GNU_SOURCE
-#    define _GNU_SOURCE 1
-#  endif
-#
-#  ifndef __USE_XOPEN
-#    define __USE_XOPEN 1
-#  endif
-#
-#else
-#  define _BSD_SOURCE 1
-#endif
-
 #include <time.h>
 
 #include "defines.h"

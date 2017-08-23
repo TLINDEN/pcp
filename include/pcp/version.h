7e54d2701183e4791ad305cc8ba2ed08bcbb2771
Thomas von Dein <tom@izb.net>
2017-08-23 13:00:12 +0200

    version bump to 0.4.1

--- include/pcp/version.h
+++ include/pcp/version.h
@@ -1,7 +1,7 @@
 /*
     This file is part of Pretty Curved Privacy (pcp1).
 
-    Copyright (C) 2013-2016 T.v.Dein.
+    Copyright (C) 2013-2017 T.v.Dein.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 
 #define PCP_VERSION_MAJOR 0
 #define PCP_VERSION_MINOR 4
-#define PCP_VERSION_PATCH 0
+#define PCP_VERSION_PATCH 1
 
 #define PCP_VERSION PCP_MAKE_VERSION(PCP_VERSION_MAJOR, PCP_VERSION_MINOR, PCP_VERSION_PATCH)
 #define PCP_MAKE_VERSION(major, minor, patch) ((major * 10000) + (minor * 100) + (patch))
